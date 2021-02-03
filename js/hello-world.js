ae17e6b2d6ab9d852e8b22145e977a14977e0ddd
Estibaliz Barato <estibalizbarato@gmail.com>
2021-02-03 17:08:04 +0100

    install and compile

---
+++ js/hello-world.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("hola mundo soy esti y estoy aprendiendo typescript");
+alert("hola mundo soy esti y estoy aprendiendo ts");
---
+++ ts/hello-world.ts
@@ -0,0 +1,2 @@
+console.log("hola mundo soy esti y estoy aprendiendo typescript");
+alert("hola mundo soy esti y estoy aprendiendo ts");
